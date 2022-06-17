3b0d5e1d05217c3e1dc37c8569b26044a79fed30
lirioblanko <goldliliya@mail.ru>
2022-06-17 08:43:24 +0300

    wrapper (general styles)

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import './Card.css'
+
+export const  Card = ({children, className}) => {
+    const classes='card ' + className
+
+    return (
+        <div className={classes}>{children}</div>
+    )
+}
